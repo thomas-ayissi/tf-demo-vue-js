c503c9a1643e121f1acb2e4fee8c5abbf136073a
Aude Beurive <aude.beurive@bstorm.be>
2022-05-06 16:29:32 +0200

    style homepage

--- VueDemo/src/views/HomeView.vue
+++ VueDemo/src/views/HomeView.vue
@@ -2,6 +2,82 @@
 </script>
 
 <template>
-  <main>
-  </main>
+  <h1>Documentation</h1>
+  <div class="demoflex">
+    <div class="demoflexcol w-96">
+      <h2>Les documentations officielles :</h2>
+      <div class="demoflex">
+        <img class="w-20 mr-3" src="../assets/logo.svg" alt="">
+        <div>
+          <h3><a href="https://vuejs.org/">Documentation VueJS</a></h3>
+          <div>Documentation officielle de VueJS version 3</div>
+        </div>
+      </div>
+      <div class="demoflex">
+      <img class="w-20 mr-3" src="../assets/images/vitelogo.png" alt="">
+      <div>
+      <h3><a href="https://vitejs.dev/guide/">Documentation Vite</a></h3>
+      <div>Documentation officielle de Vite, le bundler développé par le créateur de Vue</div>
+      </div>
+      </div>
+      <div class="demoflex">
+        <img class="w-20 mr-3" src="../assets/images/pinia.svg" alt="">
+        <div>
+          <h3><a href="https://pinia.vuejs.org/">Documentation Pinia</a></h3>
+          <div>Pinia est le gestionnaire de state recommandé par la team Vue (anciennement VueX)</div>
+        </div>
+      </div>
+      <div class="demoflex">
+        <img class="w-20 mr-3" src="../assets/logo.svg" alt="">
+        <div>
+          <h3><a href="https://router.vuejs.org/">Documentation VueRouter</a></h3>
+          <div>Documentation de VueRouter, le routing officiel de VueJs</div>
+        </div>
+      </div>
+    </div>
+    <div class="demoflexcol w-96">
+      <h2>Les gens à suivre :</h2>
+      <div class="demoflex">
+        <img class="w-20 rounded-full mr-3" src="../assets/images/evanyou.jpg" alt="">
+        <div>
+          <h3><a href="https://twitter.com/youyuxi">Evan You</a></h3>
+          <div>Créateur de VueJS et de Vite, très actif sur les TS, partage les nouveautés</div>
+        </div>
+      </div>
+      <div class="demoflex">
+        <img class="w-20 rounded-full mr-3" src="../assets/images/anthonyfu.jpg" alt="">
+        <div>
+          <h3><a href="https://twitter.com/antfu7">Anthony Fu</a></h3>
+          <div>Membre de la team Vue, très actif sur les réseaux en dev open source</div>
+        </div>
+      </div>
+      <div class="demoflex">
+        <img class="w-20 rounded-full mr-3" src="../assets/images/guillaumechau.jpg" alt="">
+        <div>
+          <h3><a href="https://twitter.com/Akryum">Guillaume Chau</a></h3>
+          <div>Membre de la team Vue, francophone, très actif sur les RS</div>
+        </div>
+      </div>
+       <div class="demoflex">
+        <img class="w-20 rounded-full mr-3" src="../assets/images/eduardo.jpg" alt="">
+        <div>
+          <h3><a href="https://twitter.com/posva">Eduardo</a></h3>
+          <div>Membre de la team Vue, francophone, surtout sur VueX et Pinia</div>
+        </div>
+      </div>
+      <div class="demoflex">
+        <img class="w-20 rounded-full mr-3" src="../assets/images/michaelthiessen.jpg" alt="">
+        <div>
+          <h3><a href="https://twitter.com/MichaelThiessen">Michael Thiessen</a></h3>
+          <div>Tips et cours sur Vue et newsletter hebdomadaire</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+a:hover {
+  @apply text-orange-400
+}
+</style>
